8eeeb0cfeb27e6c5514afc2ef08df11207dfe3fa
Askolini <asmircc@gmail.com>
2021-09-22 14:32:01 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+var myPet = {
+    species: "Tornjak",
+    name: "Snoopie",
+    legs: 4,
+    friends: ['Astor', 'Pujdo']
+}
+
+function myFunction(myObj) {
+    return myObj;
+}
+
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
